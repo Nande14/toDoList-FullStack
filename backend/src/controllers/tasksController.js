0f3b96ab53e3fe8cb700a58690001f976236b0af
Nande14 <nande.nave@gmail.com>
2023-11-09 16:30:10 -0300

    Criação do POST

--- backend/src/controllers/tasksController.js
+++ backend/src/controllers/tasksController.js
@@ -1,12 +1,20 @@
+const { response } = require('express');
 const tasksModels = require('../models/tasksModels');
 
-const getAll = async (request, response) => {
+const getAll = async (_request, response) => {
     
     const tasks = await tasksModels.getAll();
     
     return response.status(200).json(tasks);
 };
 
+const createTasks = async (request, response) => {
+
+const createTasks = await tasksModels.createTasks(request.body);
+return response.status(201).json(createTasks);
+};
+
 module.exports = {
-    getAll
+    getAll,
+    createTasks,
 }
--- backend/src/models/tasksModels.js
+++ backend/src/models/tasksModels.js
@@ -2,9 +2,23 @@ const connection = require('./connection');
 
 const getAll = async() => {
     const [tasks] = await connection.execute('SELECT * FROM tasks');
-    return tasks[0];
+    return tasks;
+};
+
+const createTasks = async (tasks) => {
+
+    const {title} = tasks;
+    
+    const dateUTC = new Date(Date.now()).toUTCString();
+
+    const query = 'INSERT INTO tasks(title, status,created_at) VALUES(?, ?, ?)';
+
+    const [createTasks] = await connection.execute(query, [title,'pendente' , dateUTC]);
+
+    return {insertId: createTasks.insertId};
 };
 
 module.exports = {
-    getAll
+    getAll,
+    createTasks,
 };
